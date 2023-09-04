776eb382bbad40ebe1609378e993d044024563e5
Joao Paulo <joaopaulojpsp@gmail.com>
2023-09-04 13:51:02 -0300

    Importações importantes

--- app/src/main/java/com/codigodasantidade/crucificado/CodigoSantidade.kt
+++ app/src/main/java/com/codigodasantidade/crucificado/CodigoSantidade.kt
@@ -2,14 +2,8 @@ package com.codigodasantidade.crucificado
 
 import android.app.Activity
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.codigodasantidade.cruxificado.R
-import com.codigodasantidade.cruxificado.databinding.ActivityCodigoSantidadeBinding
+
 
 class CodigoSantidade : Activity() {
 
--- app/src/main/java/com/codigodasantidade/crucificado/MainActivity.kt
+++ app/src/main/java/com/codigodasantidade/crucificado/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import com.codigodasantidade.cruxificado.R
 
 class MainActivity : Activity() {
 
