0b5c0fd988786e32f292ad5035d34b7ff3875902
Joao Paulo <joaopaulojpsp@gmail.com>
2023-09-04 12:58:37 -0300

    Adição de imagens e atualização do codigo

---
+++ app/src/main/java/com/codigodasantidade/cruxificado/Game_Over.kt
@@ -0,0 +1,11 @@
+package com.codigodasantidade.cruxificado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Game_Over : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_over)
+    }
+}
