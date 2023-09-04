e19649ac8ef0196cd56dffb8a4a73d3e93e95e98
Joao Paulo <joaopaulojpsp@gmail.com>
2023-09-04 13:48:31 -0300

    Atualização importante

--- app/src/main/java/com/codigodasantidade/crucificado/Game_Over.kt
+++ app/src/main/java/com/codigodasantidade/crucificado/Game_Over.kt
@@ -2,6 +2,7 @@ package com.codigodasantidade.crucificado
 
 import android.app.Activity
 import android.os.Bundle
+import com.codigodasantidade.cruxificado.R
 
 class Game_Over : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
